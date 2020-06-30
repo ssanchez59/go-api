ced0246fd46ba785b3e510c453c73f898f05118c
Sebastian <sebastian@aktek.io>
2020-06-30 16:37:29 -0500

    sending get request to ssllabs

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/buaazp/fasthttprouter"
 	"github.com/valyala/fasthttp"
@@ -13,13 +16,33 @@ func Index(ctx *fasthttp.RequestCtx) {
 }
 
 func Hello(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("name"))
+	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("domain"))
+	domain := ctx.UserValue("domain").(string)
+
+	url := "https://api.ssllabs.com/api/v3/analyze?host=" + domain
+	fmt.Println("URL:>", url)
+
+	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(jsonStr))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
 }
 
 func main() {
 	router := fasthttprouter.New()
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+	router.GET("/hello/:domain", Hello)
 
 	log.Println("Listening on localhost:8080")
 
