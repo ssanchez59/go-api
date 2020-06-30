fbbb9d3d31ea09b006e861c89e5a13c4534f8f9f
Sebastian <sebastian@aktek.io>
2020-06-30 16:11:02 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ssanchez59/go-api
+
+go 1.13
+
+require (
+	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/valyala/fasthttp v1.14.0
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
+)
+
+func Index(ctx *fasthttp.RequestCtx) {
+	fmt.Fprint(ctx, "Welcome!\n")
+}
+
+func Hello(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("name"))
+}
+
+func main() {
+	router := fasthttprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Println("Listening on localhost:8080")
+
+	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+}
