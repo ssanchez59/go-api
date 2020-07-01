dc5bd2c795b93da8f8a9726962c6ecd9759d4c81
Sebastian <sebastian@aktek.io>
2020-07-01 09:18:31 -0500

    adding domains to servers table

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"net/http"
 
 	"github.com/buaazp/fasthttprouter"
+	"github.com/lib/pq"
 	"github.com/valyala/fasthttp"
 )
 
@@ -43,6 +45,28 @@ func Index(ctx *fasthttp.RequestCtx) {
 func Hello(ctx *fasthttp.RequestCtx) {
 	// fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("domain"))
 	domain := ctx.UserValue("domain").(string)
+
+	// Connect to the "api_info" database.
+	db, err := sql.Open("postgres",
+		"postgresql://maxroach@localhost:26257/api_info?ssl=true&sslmode=require&sslrootcert=certs/ca.crt&sslkey=certs/client.maxroach.key&sslcert=certs/client.maxroach.crt")
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	defer db.Close()
+
+	// Create the "servers" table.
+	if _, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS servers (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), domain STRING )"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Insert domain into the "servers" table.
+	tblname := "servers"
+	quoted := pq.QuoteIdentifier(tblname)
+	if _, err := db.Exec(
+		fmt.Sprintf("INSERT INTO %s (domain) VALUES ($1)", quoted), domain); err != nil {
+		log.Fatal(err)
+	}
 
 	url := "https://api.ssllabs.com/api/v3/analyze?host=" + domain
 	fmt.Println("URL:>", url)
@@ -82,7 +106,7 @@ func main() {
 	router.GET("/", Index)
 	router.GET("/hello/:domain", Hello)
 
-	log.Println("Listening on localhost:8080")
+	log.Println("Listening on localhost:8000")
 
-	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+	log.Fatal(fasthttp.ListenAndServe(":8000", router.Handler))
 }
